8fd677bcde995d46eca9aeefe0b53d899361397c
Robert Kolner <robert.kolner@gmail.com>
2013-08-04 17:05:12 +0200

    Proof of concept.

---
+++ imaginate/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+from views import IndexView
+
+urlpatterns = patterns('',
+    url(r'^image/(.*)$', IndexView.as_view(), name='home'),
+)
---
+++ imaginate/views.py
@@ -0,0 +1,97 @@
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.views.generic import TemplateView
+from urlparse import urlparse
+
+import errno
+import os
+import subprocess
+
+class IndexView(TemplateView):
+    phantomjs_path = "./imaginate/phantomjs"
+    var_path = "/var/tmp/"
+    
+    def get(self, request, url):
+        no_cache = request.GET.get("no_cache") == "1"
+
+        image = self._get_image(url, no_cache)
+
+        try:
+            with open(image, "rb") as f:
+                return HttpResponse(f.read(), mimetype="image/jpeg")
+        except IOError: 
+            raise Http404
+
+    def _get_cachedir(self):
+        path = self.var_path + "imaginate/"
+    
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    
+        return path
+    
+    def _get_filename(self, url):
+        if len(url) == 0:
+            raise ValueError("You must provide url.")
+    
+        parsed = urlparse(url)
+        path = parsed.netloc + parsed.path
+        if path[-1] == '/':
+            path = path[:-1]
+
+        return "{}.png".format(path)
+    
+    def _create_image(self, url, output_path):
+        width = 480
+        height = 240
+        script = \
+    """ 
+    var page = require("webpage").create();
+    var url = "{url}";
+    var targetWidth = {width};
+    var targetHeight = {height};
+    
+    page.viewportSize = {{
+        width: 1366,
+        height: 768
+    }};
+
+    page.open(url, function(status) {{
+
+        var pageWidth = page.evaluate(function() {{
+            return document.width;
+        }});
+
+        var zoomFactor = targetWidth / pageWidth;
+        page.clipRect = {{
+            top: 0,
+            left: 0,
+            width: targetWidth / zoomFactor,
+            height: targetHeight / zoomFactor
+        }}
+
+        page.render("{output}");
+        phantom.exit();
+    }});
+    """.format(url=url, output=output_path, width=width, height=height)
+        tmp_script_path = self._get_cachedir() + 'script.js'
+        tmp_script = open(tmp_script_path, 'w')
+        tmp_script.write(script)
+        tmp_script.close()
+    
+        return subprocess.check_call([self.phantomjs_path, tmp_script_path])
+    
+    def _get_image(self, url, no_cache=False):
+        cachedir = self._get_cachedir()
+        filename = self._get_filename(url)
+        path = cachedir + filename
+    
+    
+        if no_cache or not os.path.exists(path):
+            self._create_image(url, path)
+    
+        return path
+
